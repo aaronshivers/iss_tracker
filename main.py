55e0929692feee4fd25d450640117ba7a4556a75
aaronshivers <aaron@moonmail.org>
2019-12-29 09:19:57 -0600

    moved everything inside of a class

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ class IssTracker:
 
     def run_iss_tracker(self):
         self.get_people()
+        self.draw_huston()
+        self.draw_independence()
         while True:
             self._update_screen()
 
@@ -57,84 +59,53 @@ class IssTracker:
         iss.penup()
         iss.goto(lon, lat)
 
+    def draw_huston(self):
+        # space center, Huston
+        lat = 29.5502
+        lon = -95.097
+
+        location = turtle.Turtle()
+        location.penup()
+        location.color('yellow')
+        location.goto(lon, lat)
+        location.dot(5)
+        location.hideturtle()
+
+        url = 'http://api.open-notify.org/iss-pass.json'
+        url = url + '?lat=' + str(lat) + '&lon=' + str(lon)
+        response = urllib.request.urlopen(url)
+        result = json.loads(response.read())
+
+        over = result['response'][1]['risetime']
+
+        # print over
+        style = ('Arial', 6, 'bold')
+        location.write(time.ctime(over), font=style)
+
+    def draw_independence(self):
+        # Independence, MO
+        lat = 39.091118
+        lon = -94.415504
+
+        location = turtle.Turtle()
+        location.penup()
+        location.color('yellow')
+        location.goto(lon, lat)
+        location.dot(5)
+        location.hideturtle()
+
+        url = 'http://api.open-notify.org/iss-pass.json'
+        url = url + '?lat=' + str(lat) + '&lon=' + str(lon)
+        response = urllib.request.urlopen(url)
+        result = json.loads(response.read())
+
+        over = result['response'][1]['risetime']
+
+        # print over
+        style = ('Arial', 6, 'bold')
+        location.write(time.ctime(over), font=style)
+
 
 if __name__ == '__main__':
     iss_tracker = IssTracker()
     iss_tracker.run_iss_tracker()
-
-# url = 'http://api.open-notify.org/astros.json'
-# response = urllib.request.urlopen(url)
-# result = json.loads(response.read())
-# print('People in Space:', result['number'])
-#
-# people = result['people']
-#
-# for p in people:
-#     print(p['name'], 'in', p['craft'])
-#
-# url = 'http://api.open-notify.org/iss-now.json'
-# response = urllib.request.urlopen(url)
-# result = json.loads(response.read())
-#
-# location = result['iss_position']
-# lat = float(location['latitude'])
-# lon = float(location['longitude'])
-# print('Latitude:', lat)
-# print('Longitude:', lon)
-
-# screen = turtle.Screen()
-# screen.setup(720, 360)
-# screen.setworldcoordinates(-180, -90, 180, 90)
-# screen.bgpic('map.gif')
-
-# screen.register_shape('iss2.gif')
-# iss = turtle.Turtle()
-# iss.shape('iss2.gif')
-# iss.setheading(90)
-#
-# iss.penup()
-# iss.goto(lon, lat)
-
-# space center, Huston
-lat = 29.5502
-lon = -95.097
-
-location = turtle.Turtle()
-location.penup()
-location.color('yellow')
-location.goto(lon, lat)
-location.dot(5)
-location.hideturtle()
-
-url = 'http://api.open-notify.org/iss-pass.json'
-url = url + '?lat=' + str(lat) + '&lon=' + str(lon)
-response = urllib.request.urlopen(url)
-result = json.loads(response.read())
-
-over = result['response'][1]['risetime']
-
-# print over
-style = ('Arial', 6, 'bold')
-location.write(time.ctime(over), font=style)
-
-# Independence, MO
-lat = 39.091118
-lon = -94.415504
-
-location = turtle.Turtle()
-location.penup()
-location.color('yellow')
-location.goto(lon, lat)
-location.dot(5)
-location.hideturtle()
-
-url = 'http://api.open-notify.org/iss-pass.json'
-url = url + '?lat=' + str(lat) + '&lon=' + str(lon)
-response = urllib.request.urlopen(url)
-result = json.loads(response.read())
-
-over = result['response'][1]['risetime']
-
-# print over
-style = ('Arial', 6, 'bold')
-location.write(time.ctime(over), font=style)
